ca0cea9614f80fb856dd73ffdc358c0f2307ba36
Sergio Perez <sergiodperezp@gmail.com>
2018-10-23 00:41:45 -0400

    addes service worker

---
+++ sw.js
@@ -0,0 +1,416 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "assets/css/basic.min.css",
+    "revision": "0dbdec3b3da9422bb76a2f7af70e0c1b"
+  },
+  {
+    "url": "assets/css/style.css",
+    "revision": "1ab805bf254c011f6e2d53c33d6423ee"
+  },
+  {
+    "url": "assets/data/apex.json",
+    "revision": "eb0f7339c4e16acaa607205966732ff0"
+  },
+  {
+    "url": "assets/data/dsjga.json",
+    "revision": "60b3ecd7610159fa60100ee4c1911632"
+  },
+  {
+    "url": "assets/data/kikes.json",
+    "revision": "f2f05ba0c5649e1d6bb599a87fa4de66"
+  },
+  {
+    "url": "assets/data/links.json",
+    "revision": "73d85b69295f783b194c18cd80f96241"
+  },
+  {
+    "url": "assets/data/sergiod.json",
+    "revision": "d3ca9dab4172287f0a69863e1d571185"
+  },
+  {
+    "url": "assets/data/sysandarc.json",
+    "revision": "a0015e70ccf365758ba7e17479fddd03"
+  },
+  {
+    "url": "assets/data/tripoli.json",
+    "revision": "f1d376949abb4c7f7aa0536762eab0d0"
+  },
+  {
+    "url": "assets/icons/icon-144.png",
+    "revision": "317ce9f1f182edbc8c4ea0e4e414240b"
+  },
+  {
+    "url": "assets/icons/icon-192.png",
+    "revision": "9b403208fb8c7861d51af44b6515f1cb"
+  },
+  {
+    "url": "assets/icons/icon-240.png",
+    "revision": "b319022d19bba21c1ea47e3fb858ccbd"
+  },
+  {
+    "url": "assets/icons/icon-48.png",
+    "revision": "fa4303ea66e09bb879c2a1bb4a381ed1"
+  },
+  {
+    "url": "assets/icons/icon-512.png",
+    "revision": "ffd551e47fb87e37efc3c7c8be27c2fb"
+  },
+  {
+    "url": "assets/icons/icon-96.png",
+    "revision": "350ee193dca5989e541397a5b1baed4f"
+  },
+  {
+    "url": "assets/images/business/google-my-business.png",
+    "revision": "2f253ea23b32a0125d0e920b956bf7aa"
+  },
+  {
+    "url": "assets/images/business/thumbtack.png",
+    "revision": "5ab3c23a224441907b4fd27536a42c58"
+  },
+  {
+    "url": "assets/images/business/wave.png",
+    "revision": "fbefa6371b172d1a420ccdaad2df3b90"
+  },
+  {
+    "url": "assets/images/business/yelp-for-business.png",
+    "revision": "78d96a1d7dc620f49a3362905a117c7b"
+  },
+  {
+    "url": "assets/images/cloud/dropbox.png",
+    "revision": "8c14ca794529079eaebbf9ba6c019b61"
+  },
+  {
+    "url": "assets/images/cloud/google-drive.png",
+    "revision": "492ff90bde267db143f7476be0345c6e"
+  },
+  {
+    "url": "assets/images/cloud/onedrive.png",
+    "revision": "70f60644b008377130567a1f1bbced2a"
+  },
+  {
+    "url": "assets/images/communication/android-messages.png",
+    "revision": "10f7721b56757449c1f963b89ecf8b3b"
+  },
+  {
+    "url": "assets/images/communication/groupme.png",
+    "revision": "d430cbf849bbbb8f33fe518199c9cbb0"
+  },
+  {
+    "url": "assets/images/communication/messenger.png",
+    "revision": "786c98dabdb6899b75fdc73f365abe0d"
+  },
+  {
+    "url": "assets/images/communication/skype.png",
+    "revision": "260097bf275ca3cc33faa1ba4fc1fdeb"
+  },
+  {
+    "url": "assets/images/communication/whatsapp.png",
+    "revision": "86f973cacea6f4647cff2bd012850e15"
+  },
+  {
+    "url": "assets/images/education/codecademy.png",
+    "revision": "abb0763ee3282fb5af8503a2c7c46731"
+  },
+  {
+    "url": "assets/images/education/duolingo.png",
+    "revision": "7cf5b17ed90386e56603d52dfc560d18"
+  },
+  {
+    "url": "assets/images/education/edx.png",
+    "revision": "13cfb6f1bf6bedbc84039ad9c259ce2d"
+  },
+  {
+    "url": "assets/images/education/egghead.png",
+    "revision": "244f74b5b9e44dcfab3d07613d322f47"
+  },
+  {
+    "url": "assets/images/education/lynda.png",
+    "revision": "d5b3d1e65a826b3f39b1b67e29bccfb9"
+  },
+  {
+    "url": "assets/images/education/mongodb-university.png",
+    "revision": "1bce1dab4a3bb83b819909c842d5c444"
+  },
+  {
+    "url": "assets/images/email/gmail.png",
+    "revision": "60dcb8955a975f5f7860ceb10dd58a3b"
+  },
+  {
+    "url": "assets/images/email/outlook.png",
+    "revision": "36583fc268c6644255b105fee4b2d4f1"
+  },
+  {
+    "url": "assets/images/email/roundcube.png",
+    "revision": "abbddb57801395de9c1cf41bb0b5ef84"
+  },
+  {
+    "url": "assets/images/email/yahoo-mail.png",
+    "revision": "7812ab0a2a90039330148f2a980c671a"
+  },
+  {
+    "url": "assets/images/email/zoho-mail.png",
+    "revision": "18875787b6595ade6144107972f1a839"
+  },
+  {
+    "url": "assets/images/entertainment/netflix.png",
+    "revision": "06f57ac8b4274c1fbdee6014bb6db0a5"
+  },
+  {
+    "url": "assets/images/entertainment/youtube.png",
+    "revision": "c96386951e661e4d93c8dc22edfe9ae0"
+  },
+  {
+    "url": "assets/images/finances/bank-of-america.png",
+    "revision": "a2e2e9f75aa3d8c28e98da9aa46d518e"
+  },
+  {
+    "url": "assets/images/finances/chase.png",
+    "revision": "dd954dfd453ded40e62b8e3d98770da7"
+  },
+  {
+    "url": "assets/images/finances/paypal.png",
+    "revision": "fc035d14579b7c1c0ea0831289f18ecc"
+  },
+  {
+    "url": "assets/images/hosting/aws.png",
+    "revision": "e467a0754f0f1043dacc09699b204441"
+  },
+  {
+    "url": "assets/images/hosting/firebase.png",
+    "revision": "4f617cd2542999c5b7cf1d075b3d0d12"
+  },
+  {
+    "url": "assets/images/marketing/mailchimp.png",
+    "revision": "0302db69666be940fd97277127e44e1a"
+  },
+  {
+    "url": "assets/images/music/pandora.png",
+    "revision": "cfacc5c93897e967da646bc1d28dbb5a"
+  },
+  {
+    "url": "assets/images/music/soundcloud.png",
+    "revision": "e1b0c6e9d010e042222bd0f5e95776d3"
+  },
+  {
+    "url": "assets/images/music/spotify.png",
+    "revision": "df9c96858f8b428bb61ae774b8ca4a70"
+  },
+  {
+    "url": "assets/images/others/apex-furniture-refinishing.png",
+    "revision": "a0c4a5317d270e49a81db8db7246d3f4"
+  },
+  {
+    "url": "assets/images/others/dsjga.png",
+    "revision": "625e66b27abcc732b230cc49d6ab0b46"
+  },
+  {
+    "url": "assets/images/others/ipower.png",
+    "revision": "d02fd04ca64367682afb3d25fa05ed1d"
+  },
+  {
+    "url": "assets/images/others/kikes-tree-service.png",
+    "revision": "0618a0428f294b1f3811ccaf2c996bbc"
+  },
+  {
+    "url": "assets/images/others/netgear.png",
+    "revision": "d10f3a79ee562f15bdeb4996c18f3b03"
+  },
+  {
+    "url": "assets/images/others/sysandarc.png",
+    "revision": "4ff2b46ef137ed7d572d93c7bcc14921"
+  },
+  {
+    "url": "assets/images/others/tripoli.png",
+    "revision": "de6de2abb9e7d45902633abd60be8e93"
+  },
+  {
+    "url": "assets/images/others/webpack.png",
+    "revision": "886153430e6bfb5ca800f7b1d3b80061"
+  },
+  {
+    "url": "assets/images/others/wordpress.png",
+    "revision": "47523ea94aa4a14a14589e5a294244d8"
+  },
+  {
+    "url": "assets/images/photography/google-photos.png",
+    "revision": "5a0559080357de97baec35d08f6aa476"
+  },
+  {
+    "url": "assets/images/productivity/google-keep.png",
+    "revision": "8fa60671cb3fedafd8ac9dbf04b767b0"
+  },
+  {
+    "url": "assets/images/productivity/todoist.png",
+    "revision": "619fff03c6d638c6315fee0bc93f86af"
+  },
+  {
+    "url": "assets/images/reference/wikipedia.png",
+    "revision": "01c87867870c7c042e9d0cd3c819cbff"
+  },
+  {
+    "url": "assets/images/search/bing.png",
+    "revision": "047a2234ec00b824eaff1fc07c245acd"
+  },
+  {
+    "url": "assets/images/search/google.png",
+    "revision": "93797aad706bfde2e882a4a57b2cce7f"
+  },
+  {
+    "url": "assets/images/search/yahoo.png",
+    "revision": "621d7b3e0ae0e250c2e3d0262ea4e65c"
+  },
+  {
+    "url": "assets/images/shopping/amazon.png",
+    "revision": "3e010bc37db58af4a47156d94da00f3c"
+  },
+  {
+    "url": "assets/images/shopping/ebay.png",
+    "revision": "1e75d450bb4bda6b74a01f9d52516e25"
+  },
+  {
+    "url": "assets/images/social/angies-list.png",
+    "revision": "562f34a5ccd4d3a5b7822fb4993ebaf8"
+  },
+  {
+    "url": "assets/images/social/bbb.png",
+    "revision": "a68ecee6ea36d8cf59df1e6b7152eb69"
+  },
+  {
+    "url": "assets/images/social/facebook.png",
+    "revision": "38ead329c860be61a21239aea6bf6b92"
+  },
+  {
+    "url": "assets/images/social/google.png",
+    "revision": "93797aad706bfde2e882a4a57b2cce7f"
+  },
+  {
+    "url": "assets/images/social/instagram.png",
+    "revision": "62491320140fc9280cf2e1a90909b836"
+  },
+  {
+    "url": "assets/images/social/kudzu.png",
+    "revision": "78cc4332375e67ad5f288c0a6c3366f8"
+  },
+  {
+    "url": "assets/images/social/pinterest.png",
+    "revision": "98da3a81155d7e32111114e121a6397d"
+  },
+  {
+    "url": "assets/images/social/snapchat.png",
+    "revision": "ed7a33c44004e118694b410bc39e0d79"
+  },
+  {
+    "url": "assets/images/social/twitter.png",
+    "revision": "c89f3eb0d4bd97847d88fac3d8e41b63"
+  },
+  {
+    "url": "assets/images/social/yelp.png",
+    "revision": "9e521f993b67665d6766204d35960b7b"
+  },
+  {
+    "url": "assets/images/tech/codepen.png",
+    "revision": "be224435f8eb99c2a6060a801ab5e0cb"
+  },
+  {
+    "url": "assets/images/tech/github.png",
+    "revision": "402ebcc6b19a5a36f81155ebea488784"
+  },
+  {
+    "url": "assets/images/tools/google-maps.png",
+    "revision": "990f181e4990b64e1cc5f3d06a198b7a"
+  },
+  {
+    "url": "assets/images/tools/google-search-console.png",
+    "revision": "93797aad706bfde2e882a4a57b2cce7f"
+  },
+  {
+    "url": "assets/images/tools/google-translate.png",
+    "revision": "1b909e282e196de280aa99061970a5ab"
+  },
+  {
+    "url": "assets/images/tools/waze.png",
+    "revision": "5afc276e4ef02cec8fcc08e1b607edf7"
+  },
+  {
+    "url": "assets/js/script.js",
+    "revision": "d34a406886487ea63637ce66bd768904"
+  },
+  {
+    "url": "favicon.ico",
+    "revision": "315a845d22e6007c09b496beed7eecdb"
+  },
+  {
+    "url": "index.html",
+    "revision": "7e78a46b4d8ef321869674e176afa0f7"
+  },
+  {
+    "url": "libs/modules/footer.js",
+    "revision": "ccf4680718a0535e4373fe256291b431"
+  },
+  {
+    "url": "libs/modules/links.js",
+    "revision": "bc9e72c6c865bd3991ca2baa20ae3199"
+  },
+  {
+    "url": "libs/script.js",
+    "revision": "4c273c8d3defa299d1091114a1c0154c"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "a0f171aa7324a5d9a6440bc895df9039"
+  },
+  {
+    "url": "robots.txt",
+    "revision": "b3b8d14570e4dee1ef86b7abe3a6ee5b"
+  },
+  {
+    "url": "user/apex-furniture-refinishing.html",
+    "revision": "dd4f9374cbaff04e38c8a2b65f4f7504"
+  },
+  {
+    "url": "user/dsjga.html",
+    "revision": "8af4c0560f6ad3dc8188a061bd558bee"
+  },
+  {
+    "url": "user/head.html",
+    "revision": "b5501a0b85732c049f5cead46b668c60"
+  },
+  {
+    "url": "user/kikes-tree-service.html",
+    "revision": "a33404f813309eb30a874939b6c758d6"
+  },
+  {
+    "url": "user/sergio-perez.html",
+    "revision": "40a5e96872445948bcc65be26d2f0927"
+  },
+  {
+    "url": "user/systems-and-architecture.html",
+    "revision": "0ee6ec7243e25295b9909206eff78b71"
+  },
+  {
+    "url": "user/tripoli-halal-meat.html",
+    "revision": "6eb370bc6af9518ee5ac8a70548a970a"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
