3d39ec2d4a7ce56a7eb49f33753db7da67104127
Systems & Architecture <systemsandarchitecture@gmail.com>
2024-05-01 16:51:26 -0400

    Update Google Analytics

--- assets/js/head.js
+++ assets/js/head.js
@@ -7,22 +7,24 @@ export function head(headTitle) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
     <title>${headTitle}</title>
-    <meta name="theme-color" content="#0050b4">
+    <meta name="theme-color" content="#001848">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="msapplication-navbutton-color" content="#0050b4">
+    <meta name="msapplication-navbutton-color" content="#001848">
     <meta name="msapplication-starturl" content="./">
     <link href="../assets/css/basic.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,700,900" rel="stylesheet">
-    <link href="../assets/css/style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@200..900&family=Raleway:wght@900&display=swap" rel="stylesheet">
+      <link href="../assets/css/style.css" rel="stylesheet">
     <link href="../favicon.ico" rel="icon">
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141701136-1"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FQVBGZ3MR7"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', 'UA-141701136-1');
+      gtag('config', 'G-FQVBGZ3MR7');
     </script>
-  `;
+    `;
 
   head.innerHTML = headStructure;
 }
